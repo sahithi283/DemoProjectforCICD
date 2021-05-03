3ad228bd5c3a91c8083962931de432680a752583
Sahithi_Kakkerla@epam.com <StayDetermined@143>
2021-05-03 18:01:09 +0530

    my maven project

---
+++ src/test/java/Base.java
@@ -0,0 +1,15 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Base {
+
+    @Test
+    public void f() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void f1() {
+        Assert.assertTrue(true);
+    }
+}
